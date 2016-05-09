47c7a5a2aba0fc3664041ea037e5a93bc5c0dc8f
Will Riley <will@hflw.org>
2016-05-09 01:29:58 -0700

    Moved buttom + display into new calculator component

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Button = require('./lib/components/button');
-var Display = require('./lib/components/display');
+var Calculator = require('./lib/components/calculator');
 
 var clickHandler = () => alert('hi!');
 var body = (
   <div>
     <h1>Calculator</h1>
-    <Display value="2 + 2"></Display>
-    <Button value={2} onClick={clickHandler}></Button>
+    <Calculator></Calculator>
   </div>
 )
 ReactDOM.render(body, document.getElementById('root'));
