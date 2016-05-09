e4df280e296dab8960b1d83cd47b30ac7a21dda1
Will Riley <will@hflw.org>
2016-05-09 03:41:56 -0700

    Wrote calculator fsm

--- test/fsm.js
+++ test/fsm.js
@@ -2,14 +2,15 @@ var test = require('tape');
 var Fsm = require('../lib/fsm');
 
 test('basic operations', function(t) {
+  t.plan(7);
   var fsm = Fsm.create();
   var acc = '';
-  for(var command of [1, '+', 1]) {
+  for(var command of [1, 1, '+', 1, 1]) {
     acc += command;
     var {state: fsm, output} = Fsm.pushCommand(fsm, command);
     t.equal(output, acc);
   }
   var {output} = Fsm.pushCommand(fsm, '=');
-  t.equal(output, 2);
+  t.equal(output, 22);
   t.end();
 });
