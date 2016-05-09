0d9d81c3b9d2af7bd3d0d021bae5ea0ec0ef394d
Will Riley <will@hflw.org>
2016-05-08 23:38:21 -0700

    implemented button component

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Button = require('./lib/components/button');
 
-ReactDOM.render(<h1>Calculator</h1>, document.getElementById('root'));
+var clickHandler = () => alert('hi!');
+var body = (
+  <div>
+    <h1>Calculator</h1>
+    <Button value={2} onClick={clickHandler}></Button>
+  </div>
+)
+ReactDOM.render(body, document.getElementById('root'));
