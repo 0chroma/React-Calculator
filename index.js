ff2cdf753badd34c20c904683fb4233e4e1801b8
Will Riley <will@hflw.org>
2016-05-09 00:17:07 -0700

    Added display component

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Button = require('./lib/components/button');
+var Display = require('./lib/components/display');
 
 var clickHandler = () => alert('hi!');
 var body = (
   <div>
     <h1>Calculator</h1>
+    <Display value="2 + 2"></Display>
     <Button value={2} onClick={clickHandler}></Button>
   </div>
 )
