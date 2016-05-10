c49d845e6dc55a313d8604f654ad9db1b93be07a
Will Riley <will@hflw.org>
2016-05-09 18:06:24 -0700

    Added decimal point support to fsm

--- test/fsm.js
+++ test/fsm.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var Fsm = require('../lib/fsm');
 
 test('basic operations', function(t) {
-  t.plan(7);
+  t.plan(6);
   var fsm = Fsm.create();
   var acc = '';
   for(var command of [1, 1, '+', 1, 1]) {
@@ -12,5 +12,19 @@ test('basic operations', function(t) {
   }
   var {output} = Fsm.pushCommand(fsm, '=');
   t.equal(output, 22);
+  t.end();
+});
+
+test('decimal points', function(t) {
+  t.plan(7);
+  var fsm = Fsm.create();
+  var acc = '';
+  for(var command of [1, '.', 1, '+', 1, 1]) {
+    acc += command;
+    var {state: fsm, output} = Fsm.pushCommand(fsm, command);
+    t.equal(output, acc);
+  }
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 12.1);
   t.end();
 });
