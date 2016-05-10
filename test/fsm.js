050bd493350a82e4e5228ec56a5952abd37f4fbc
Will Riley <will@hflw.org>
2016-05-09 23:31:38 -0700

    Added more test cases as a sanity check

--- test/fsm.js
+++ test/fsm.js
@@ -1,29 +1,57 @@
 var test = require('tape');
 var Fsm = require('../lib/fsm');
+var {OPS} = require('../lib/math');
 
-test('basic operations', function(t) {
-  t.plan(6);
-  var fsm = Fsm.create();
+var testCommands = function(t, fsm, commands) {
   var acc = '';
-  for(var command of [1, 1, '+', 1, 1]) {
+  for(var command of commands) {
     acc += command;
     var {state: fsm, output} = Fsm.pushCommand(fsm, command);
     t.equal(output, acc);
   }
+  return fsm
+}
+
+test('addition', function(t) {
+  t.plan(6);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [1, 1, '+', 1, 1]);
   var {output} = Fsm.pushCommand(fsm, '=');
   t.equal(output, 22);
+  t.end();
+});
+
+test('subtraction', function(t) {
+  t.plan(4);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [2, OPS.multiply, 2]);
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 4);
+  t.end();
+});
+
+test('multiplication', function(t) {
+  t.plan(4);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [2, OPS.multiply, 2]);
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 4);
+  t.end();
+});
+
+test('division', function(t) {
+  t.plan(4);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [4, OPS.divide, 2]);
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 2);
   t.end();
 });
 
 test('decimal points (middle of number)', function(t) {
   t.plan(7);
   var fsm = Fsm.create();
-  var acc = '';
-  for(var command of [1, '.', 1, '+', 1, 1]) {
-    acc += command;
-    var {state: fsm, output} = Fsm.pushCommand(fsm, command);
-    t.equal(output, acc);
-  }
+  fsm = testCommands(t, fsm, [1, '.', 1, '+', 1, 1]);
   var {output} = Fsm.pushCommand(fsm, '=');
   t.equal(output, 12.1);
   t.end();
@@ -32,13 +60,40 @@ test('decimal points (middle of number)', function(t) {
 test('decimal points (starting with)', function(t) {
   t.plan(6);
   var fsm = Fsm.create();
-  var acc = '';
-  for(var command of ['.', 0, 2, '+', 2]) {
-    acc += command;
-    var {state: fsm, output} = Fsm.pushCommand(fsm, command);
-    t.equal(output, acc);
-  }
+  fsm = testCommands(t, fsm, ['.', 0, 2, '+', 2]);
   var {output} = Fsm.pushCommand(fsm, '=');
   t.equal(output, 2.02);
+  t.end();
+});
+
+test('decimal points (rhs)', function(t) {
+  t.plan(6);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [2, '+', 2, '.', 2]);
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 4.2);
+  t.end();
+});
+
+test('del commands', function(t) {
+  t.plan(7);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [2, '+', 2, '.', 2]);
+  var {output, state: fsm} = Fsm.pushCommand(fsm, 'del');
+  t.equal(output, '2+2.');
+  var {output} = Fsm.pushCommand(fsm, 'del');
+  t.equal(output, '2+2');
+  t.end();
+});
+
+test('use output as input for next calculation', function(t) {
+  t.plan(4);
+  var fsm = Fsm.create();
+  fsm = testCommands(t, fsm, [2, '+', 2]);
+  var {state: fsm} = Fsm.pushCommand(fsm, '=');
+  var {state: fsm} = Fsm.pushCommand(fsm, '+');
+  var {state: fsm} = Fsm.pushCommand(fsm, 4);
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 8);
   t.end();
 });
