2addd6d7545b5eabd55ff51dbf9a43eab9e6f979
Will Riley <will@hflw.org>
2016-05-09 18:21:37 -0700

    Fix bug where multi-digit decimals weren't handled correctly

--- test/fsm.js
+++ test/fsm.js
@@ -15,7 +15,7 @@ test('basic operations', function(t) {
   t.end();
 });
 
-test('decimal points', function(t) {
+test('decimal points (middle of number)', function(t) {
   t.plan(7);
   var fsm = Fsm.create();
   var acc = '';
@@ -26,5 +26,19 @@ test('decimal points', function(t) {
   }
   var {output} = Fsm.pushCommand(fsm, '=');
   t.equal(output, 12.1);
+  t.end();
+});
+
+test('decimal points (starting with)', function(t) {
+  t.plan(6);
+  var fsm = Fsm.create();
+  var acc = '';
+  for(var command of ['.', 0, 2, '+', 2]) {
+    acc += command;
+    var {state: fsm, output} = Fsm.pushCommand(fsm, command);
+    t.equal(output, acc);
+  }
+  var {output} = Fsm.pushCommand(fsm, '=');
+  t.equal(output, 2.02);
   t.end();
 });
